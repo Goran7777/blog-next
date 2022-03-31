4169c858bc0ba31bc29ed9b23a94fdfabf0d86cc
Goran7777 <goranilicilke893@gmail.com>
2022-03-31 10:18:31 +0200

    f

---
+++ pages/api/upload.js
@@ -0,0 +1,36 @@
+import nextConnect from 'next-connect';
+import multer from 'multer';
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: './public/images',
+    filename: (req, file, cb) => cb(null, file.originalname),
+  }),
+});
+
+const apiRoute = nextConnect({
+  onError(error, req, res) {
+    res
+      .status(501)
+      .json({ error: `Sorry something Happened! ${error.message}` });
+    console.log('error');
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
+
+apiRoute.use(upload.single('upload'));
+
+apiRoute.post((req, res) => {
+  res.status(200).json({ data: 'success' });
+  console.log('success');
+});
+
+export default apiRoute;
+
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
